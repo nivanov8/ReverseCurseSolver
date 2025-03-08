6f85fef361b4821374219df59662d3b7b843a62e
Nikita Ivanov <nickivanov@mail.cs.toronto.edu>
2025-03-07 22:27:52 -0500

    Added conda env and basic llama

---
+++ LlamaStandard.py
@@ -0,0 +1,24 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from dotenv import load_dotenv
+from huggingface_hub import login
+import os
+
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+login(token=hf_token)
+
+cache_dir = "/scratch/expires-2025-Mar-14"
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B", cache_dir=cache_dir)
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B", cache_dir=cache_dir)
+
+
+message = "Who is the president of the United States?"
+tokens = tokenizer(message, return_tensors="pt")
+
+with torch.no_grad():
+    output = model.generate(**tokens, max_length=300, eos_token_id=tokenizer.eos_token_id)
+
+response = tokenizer.decode(output[0], skip_special_tokens=True)
+print(response)
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ReverseCurseSolver
+### Environment Setup
+1. Ensure miniconda3 is installed
+2. Source conda with ```source $YOUR_PATH/miniconda3/bin/activate```
+3. If you are creating the environment for the first time, navigate to the ReverseCurseSolver root directory run ```conda env create -f environment.yml```, if the environment is already created go to step 4
+4. Run ```conda activate LLMProject```
+
+### Hugging Face Access Token
+1. To be able to run Llama models create a local .env file in the root directory of ReverseCurseSolver
+2. In the .env file create a variable ```HF_TOKEN = $YOUR_HF_TOKEN```
+
+### Running Llama
+1. To run standard Llama, in the root directory of ReverseCurseSolver run ```python LlamaStandard.py```
