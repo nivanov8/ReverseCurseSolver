ad97d7b1423f61bbfa3fb8bb49819d524a1399ea
Kyung Jae Lee <kjlee@cs.toronto.edu>
2025-03-27 18:14:36 -0400

    Fix joint optimization fine tuning loop for experiment 1

--- joint_optimization/experiment1/finetune.py
+++ joint_optimization/experiment1/finetune.py
@@ -7,10 +7,10 @@ import torch
 from datasets import Dataset
 from dotenv import load_dotenv
 from huggingface_hub import login
-from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+from transformers import Trainer, TrainingArguments
 
-from joint_optimization.dataset import tokenize_fn, custom_collator
-from joint_optimization.model import get_paired_loss_model, get_llama_tokenizer_for_seq2seq
+from joint_optimization.dataset import tokenize_fn, pairing_collator
+from joint_optimization.model import get_llama_model_for_seq2seq, get_llama_tokenizer_for_seq2seq
 
 
 # Setup
@@ -18,9 +18,13 @@ CACHE_DIR = "/w/340/kjlee/.cache/huggingface"
 SAVE_DIR = "./joint_optimization/experiment1/llama3-finetuned-experiment1"
 MODEL_NAME = "meta-llama/Llama-3.2-3B"
 # MODEL_NAME = "meta-llama/Llama-3.2-1B"
-LEARNING_RATE = 3e-05
-NUM_EPOCHS = 5
-BATCH_SIZE = 4
+DTYPE = torch.bfloat16
+MASK_LOSS_ON_PROMPT = False
+LEARNING_RATE = 2e-05
+NUM_EPOCHS = 10
+# Effective batch size = BATCH_SIZE * GRAD_ACCUM_STEPS * 2 (since we work with pairs)
+BATCH_SIZE = 1
+GRAD_ACCUM_STEPS = 2
 
 load_dotenv()
 hf_token = os.getenv("HF_TOKEN")
@@ -28,13 +32,13 @@ login(token=hf_token)
 
 # Load model
 print("Loading model and tokenizer...")
-model = get_paired_loss_model(MODEL_NAME, torch.bfloat16, CACHE_DIR)
+model = get_llama_model_for_seq2seq(MODEL_NAME, torch_dtype=DTYPE, cache_dir=CACHE_DIR)
 tokenizer = get_llama_tokenizer_for_seq2seq(MODEL_NAME, CACHE_DIR)
 
 print("Loading dataset...")
 # Download
-TRAIN_DATASET_LINK = "https://huggingface.co/datasets/lberglund/reversal_curse/raw/main/name_description_dataset/both_prompts_train.jsonl"
-call(f"wget -O dataset.jsonl {TRAIN_DATASET_LINK}", shell=True)
+DATASET_LINK = "https://huggingface.co/datasets/lberglund/reversal_curse/raw/main/name_description_dataset/both_prompts_train.jsonl"
+call(f"wget -O dataset.jsonl {DATASET_LINK}", shell=True)
 # Preprocess
 # NOTE: The first half of the training data is in the <name> is <description> format.
 #       The second half is in the <description> is <name> format.
@@ -42,51 +46,61 @@ call(f"wget -O dataset.jsonl {TRAIN_DATASET_LINK}", shell=True)
 with open("dataset.jsonl", "r") as f:
     data = [json.loads(line) for line in f]
 
-train_dataset = []
+p2d_dataset = []
+d2p_dataset = []
 for i in range(len(data) // 2):
     name_to_desc = data[i]
     desc_to_name = data[i + len(data) // 2]
-    train_dataset.append({
-        "input1": name_to_desc["prompt"],
-        "target1": name_to_desc["completion"],
-        "input2": desc_to_name["prompt"],
-        "target2": desc_to_name["completion"],
+    p2d_dataset.append({
+        "index": i,
+        "input": name_to_desc["prompt"],
+        "target": name_to_desc["completion"],
+    })
+    d2p_dataset.append({
+        "index": i,
+        "input": desc_to_name["prompt"],
+        "target": desc_to_name["completion"],
     })
 
 # Clean up
 call(f"rm dataset.jsonl", shell=True)
 
-train_dataset = Dataset.from_list(train_dataset)
+p2d_dataset = Dataset.from_list(p2d_dataset)
+d2p_dataset = Dataset.from_list(d2p_dataset)
+p2d_dataset = p2d_dataset.train_test_split(test_size=0.2, seed=42)
 
 print("Tokenizing dataset...")
-train_dataset = train_dataset.map(partial(tokenize_fn, tokenizer))
-train_dataset.set_format(columns=[
-    'input_ids', 'attention_mask', 'labels', 
-    'input_ids_2', 'attention_mask_2', 'labels_2'
-])
+p2d_dataset = p2d_dataset.map(
+    partial(tokenize_fn, tokenizer=tokenizer),
+    remove_columns=["input", "target"],
+)
+d2p_dataset = d2p_dataset.map(
+    partial(tokenize_fn, tokenizer=tokenizer),
+    remove_columns=["input", "target"],
+)
+d2p_dataset_by_index = {data["index"] : data for data in d2p_dataset}
 
-training_args = Seq2SeqTrainingArguments(
+training_args = TrainingArguments(
     output_dir=SAVE_DIR,
     per_device_train_batch_size=BATCH_SIZE,
+    gradient_accumulation_steps=2,
     num_train_epochs=NUM_EPOCHS,
     learning_rate=LEARNING_RATE,
     save_strategy="no",
-    fp16=False,
-    bf16=True,
+    fp16=DTYPE==torch.float16,
+    bf16=DTYPE==torch.bfloat16,
     auto_find_batch_size=False,
-    eval_strategy="no",
+    eval_strategy="epoch",
     remove_unused_columns=False,
 )
 
-trainer = Seq2SeqTrainer(
+trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=train_dataset,
+    train_dataset=p2d_dataset["train"],
+    eval_dataset=p2d_dataset["test"],
     tokenizer=tokenizer,
-    data_collator=partial(custom_collator, tokenizer),
-    # NOTE: not doing eval during training
-    # eval_dataset=eval_dataset,
-    # compute_metrics=compute_metrics,
+    data_collator=partial(pairing_collator, tokenizer=tokenizer, pair_dataset=d2p_dataset_by_index),
 )
 
 # Fine-tuning
--- joint_optimization/model.py
+++ joint_optimization/model.py
@@ -2,39 +2,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from tokenizers.processors import TemplateProcessing
 
 
-def get_paired_loss_model(model_name, torch_dtype, cache_dir=None):
-    # Loads a model that can take in two inputs to compute a paired loss
-    # and is also suitable for seq2seq-type training
+def get_llama_model_for_seq2seq(model_name, torch_dtype=None, cache_dir=None):
+    # Loads a llama model suitable for seq2seq-type training
     model = AutoModelForCausalLM.from_pretrained(
         model_name, torch_dtype=torch_dtype, cache_dir=cache_dir,
         attn_implementation="sdpa",
     )
-
-    original_forward = model.forward
-
-    def paired_forward(self, input_ids=None, attention_mask=None, labels=None, 
-            input_ids_2=None, attention_mask_2=None, labels_2=None, **kwargs):
-        
-        # Compute first loss
-        outputs1 = original_forward(input_ids=input_ids, attention_mask=attention_mask, labels=labels, **kwargs)
-
-        # Single input case (inference)
-        if input_ids_2 is None:
-            return outputs1
-        
-        # Paired input case (joint optimization training)
-        loss1 = outputs1.loss
-        outputs2 = original_forward(input_ids=input_ids_2, attention_mask=attention_mask_2, labels=labels_2, **kwargs)
-        loss2 = outputs2.loss
-        total_loss = loss1 + loss2
-        outputs1.loss = total_loss
-
-        # We only need the loss value during training
-        return outputs1
-
-    # Bind paired_forward to the model's forward method
-    import types
-    model.forward = types.MethodType(paired_forward, model)
 
     # Use EOS token for padding
     model.config.pad_token_id = model.config.eos_token_id
